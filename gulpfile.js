7969714317bc798edb85e2498c9abf398f75be35
gaoshu883 <gaoshu883@163.com>
2016-12-16 21:00:28 +0800

    Use gulp to minify and concat js & css files

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,38 @@
-var gulp = require('gulp');
-var livereload = require('gulp-livereload');
-var postcss = require('gulp-postcss');
-var autoprefixer = require('autoprefixer');
-var del = require('del');
+var gulp = require('gulp'),
+    uglify = require('gulp-uglify'),
+    concatify = require('gulp-concat'),
+    sourcemaps = require('gulp-sourcemaps'),
+    cleanCSS = require('gulp-clean-css'),
+    postcss = require('gulp-postcss'),
+    autoprefixer = require('autoprefixer'),
+    del = require('del'),
+    rename = require('gulp-rename');
 
-gulp.task('autoprefixer', function () {
-    return gulp.src('./src/css/*.css')
-        .pipe(postcss([ autoprefixer({ browsers: ['last 2 versions'] }) ]))
-        .pipe(gulp.dest('./dist/css'));
+//Paths to various files
+var paths = {
+    scripts: ['js/jquery-3.1.1.min.js', 'js/bootstrap.min.js', 'js/menu.js'],
+    styles: ['css/*.css']
+};
+
+//Concats & minifies js files and outputs them to dist/js/app.js
+gulp.task('scripts', function() {
+    return gulp.src(paths.scripts)
+        // .pipe(sourcemaps.init())
+        .pipe(uglify())
+        .pipe(concatify('app.js'))
+        // .pipe(sourcemaps.write())
+        .pipe(gulp.dest('./dist/js/'));
 });
 
-gulp.task('moveJS', function () {
-  return gulp.src('./src/js/*.js')
-             .pipe(gulp.dest('./dist/js'));
+//Postprocess & minifies CSS files
+gulp.task('styles', function() {
+    return gulp.src(paths.styles)
+        .pipe(postcss([autoprefixer({ browsers: ['last 2 versions'] })]))
+        .pipe(rename({ suffix: '.min' }))
+        // .pipe(sourcemaps.init())
+        .pipe(cleanCSS())
+        // .pipe(sourcemaps.write())
+        .pipe(gulp.dest('./dist/css/'));
 });
 
-gulp.task('moveHtml', function () {
-  return gulp.src('./src/index.html')
-             .pipe(gulp.dest('./dist'));
-});
-
-gulp.task('moveImage', function () {
-  return gulp.src('./src/images/*')
-             .pipe(gulp.dest('./dist/images'));
-});
-
-gulp.task('clean:dist', function (cb) {
-  del([
-      './dist/**/*'
-    ], cb);
-});
-
-gulp.task('default', ['autoprefixer','moveJS','moveHtml','moveImage']);
+gulp.task('default', ['styles', 'scripts']);
